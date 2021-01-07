b4b8d8b5437ef72f13edb4dd7cef9a442042097f
jhertz5 <jukka.hertzog@live.co.uk>
2021-01-07 20:56:42 +0000

    Updated uart_tb.sh to stop if error is encountered and to include uart_rx

--- uart_tb.sh
+++ uart_tb.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# if anything fails, the script fails
+set -e
+
 MODULE_NAME="${0%\.*}"
 MODULE_NAME="${MODULE_NAME#\.\/}"
 WAVE_FILE="wave.ghw"
 
-mkdir work
+mkdir -p work
 cd work
 
 echo "analysing ..."
 ghdl -a --std=08 ../baud_rate_gen.vhd
 ghdl -a --std=08 ../uart_tx.vhd
+ghdl -a --std=08 ../uart_rx.vhd
 ghdl -a --std=08 ../${MODULE_NAME}.vhd
 echo "elaborating ..."
 ghdl -e --std=08 ${MODULE_NAME}
